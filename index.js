47f9c082c9f90d817d1e379ac62b53a35d4793c8
nmt2501 <ngminhtuam@gmail.com>
2026-01-24 23:18:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -84,13 +84,9 @@ app.get("/api/tx/sun", async (req, res) => {
 
     res.json({
       phien: data.phien || 0,
-      xuc_xac_1: data.xuc_xac_1 || 0,
-      xuc_xac_2: data.xuc_xac_2 || 0,
-      xuc_xac_3: data.xuc_xac_3 || 0,
-      tong: data.tong || 0,
       ket_qua: value === 1 ? "Tài" : "Xỉu",
       
-      phien_hien_tai: data.phien_hien_tai || 0,
+      phien_hien_tai: data.phien_tiep_theo || 0,
       pattern,
       du_doan: duDoanResult.du_doan,
       do_tin_cay: duDoanResult.do_tin_cay
